e95031a4cfccd748ec2e636964a8a8945122be4d
obijreo <obijr.eo@gmail.com>
2022-06-18 08:44:18 +0200

    Add files via upload
    I am unable to create a Watson Studio account so I have uploaded my assignment here (Tools for Data Science Week 4)

---
+++ Tools of Data Science - Week 4 Assignment .py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>My Jupyter Notebook on IBM Watson Studio</h1>
+# 
+
+# <b>Emmanuel Obi</b><p>
+# Compliance Data Analyst 
+
+# <i>a friend told me about a data analyst job. I did not think I qualified, however, all I need was to improve my Python skills. I thought the best was to do that was to find an online course (the same way I began learning Python years ago)</i>
+
+# <h3>My code should assign words to letters which can then be aggregated to a sentence</h3>
+
+# In[4]:
+
+
+a = "Who"
+b = "is"
+c = "the best"
+d = "Batman"
+e = " "
+f= "?"
+
+print(a+e+b+e+c+e+d+f)
+
+
+#  <ul>
+#   <li>Ben Affleck</li>
+#   <li>Robert Pattinson</li>
+#   <li>Michael Keaton</li>
+# </ul> 
+# 
+# <hr>
+# 
+# <a href="https://www.youtube.com/c/AngreiferGaming">Check out my DC Legends YouTube Channel</a>
+
+# In[ ]:
+
+
+
+
